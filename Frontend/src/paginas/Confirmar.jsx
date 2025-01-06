7db23ca0ed986448f7d8432d9a1a76ae0c9c72ef
eguachamin <evelyn.guachamin@epn.edu.ec>
2025-01-06 15:33:43 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   Backend/.env.example
    	new file:   Backend/.gitignore
    	new file:   Backend/package-lock.json
    	new file:   Backend/package.json
    	new file:   Backend/src/config/nodemailer.js
    	new file:   Backend/src/controllers/paciente_controller.js
    	new file:   Backend/src/controllers/tratamiento_controller.js
    	new file:   Backend/src/controllers/veterinario_controller.js
    	new file:   Backend/src/database.js
    	new file:   Backend/src/helpers/crearJWT.js
    	new file:   Backend/src/index.js
    	new file:   Backend/src/middlewares/autenticacion.js
    	new file:   Backend/src/middlewares/validacionVeterinario.js
    	new file:   Backend/src/models/Paciente.js
    	new file:   Backend/src/models/Tratamiento.js
    	new file:   Backend/src/models/Veterinario.js
    	new file:   Backend/src/routers/paciente_routes.js
    	new file:   Backend/src/routers/tratameinto_routes.js
    	new file:   Backend/src/routers/veterinario_routes.js
    	new file:   Backend/src/server.js
    	new file:   Frontend/.gitignore
    	new file:   Frontend/README.md
    	new file:   Frontend/index.html
    	new file:   Frontend/package-lock.json
    	new file:   Frontend/package.json
    	new file:   Frontend/postcss.config.cjs
    	new file:   Frontend/public/images/catforgot.jpg
    	new file:   Frontend/public/images/doglogin.jpg
    	new file:   Frontend/public/images/dogregister.jpg
    	new file:   Frontend/src/App.css
    	new file:   Frontend/src/App.jsx
    	new file:   Frontend/src/assets/code.png
    	new file:   Frontend/src/assets/consulting.png
    	new file:   Frontend/src/assets/dark.png
    	new file:   Frontend/src/assets/design.png
    	new file:   Frontend/src/assets/dog-hand.webp
    	new file:   Frontend/src/assets/doglost.jpg
    	new file:   Frontend/src/assets/facebook.png
    	new file:   Frontend/src/assets/fondos/captura-1.png
    	new file:   Frontend/src/assets/fondos/captura-2.png
    	new file:   Frontend/src/assets/fondos/captura-3.png
    	new file:   Frontend/src/assets/fondos/captura-4.png
    	new file:   Frontend/src/assets/fondos/captura-5.png
    	new file:   Frontend/src/assets/fondos/captura-6.png
    	new file:   Frontend/src/assets/fondos/captura-7.png
    	new file:   Frontend/src/assets/github.png
    	new file:   Frontend/src/assets/linkedin.png
    	new file:   Frontend/src/assets/rocket.webp
    	new file:   Frontend/src/assets/twitter.png
    	new file:   Frontend/src/assets/web1.png
    	new file:   Frontend/src/assets/web2.png
    	new file:   Frontend/src/assets/web3.png
    	new file:   Frontend/src/assets/web4.png
    	new file:   Frontend/src/assets/web5.png
    	new file:   Frontend/src/assets/web6.png
    	new file:   Frontend/src/assets/youtube.png
    	new file:   Frontend/src/componets/Alertas/Mensaje.jsx
    	new file:   Frontend/src/componets/Formulario.jsx
    	new file:   Frontend/src/componets/Perfil/CardPerfil.jsx
    	new file:   Frontend/src/componets/Perfil/FormularioPerfil.jsx
    	new file:   Frontend/src/componets/Tabla.jsx
    	new file:   Frontend/src/index.css
    	new file:   Frontend/src/layout/Auth.jsx
    	new file:   Frontend/src/layout/Dashboard.jsx
    	new file:   Frontend/src/main.jsx
    	new file:   Frontend/src/paginas/Actualizar.jsx
    	new file:   Frontend/src/paginas/Confirmar.jsx
    	new file:   Frontend/src/paginas/Crear.jsx
    	new file:   Frontend/src/paginas/Forgot.jsx
    	new file:   Frontend/src/paginas/LandinPage.jsx
    	new file:   Frontend/src/paginas/Listar.jsx
    	new file:   Frontend/src/paginas/Login.jsx
    	new file:   Frontend/src/paginas/NotFound.jsx
    	new file:   Frontend/src/paginas/Perfil.jsx
    	new file:   Frontend/src/paginas/Register.jsx
    	new file:   Frontend/src/paginas/Visualizar.jsx
    	new file:   Frontend/tailwind.config.cjs
    	new file:   Frontend/vite.config.js

---
+++ Frontend/src/paginas/Confirmar.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import logoDog from '../assets/dog-hand.webp'
+import {Link, useParams} from 'react-router-dom'
+import axios from 'axios'
+import Mensaje from '../componets/Alertas/Mensaje.jsx'
+
+export const Confirmar = () => {
+    //Paso 1
+    const {token} =useParams()
+    //Paso 3
+    const [mensaje , setMensaje]= useState("")   
+    //Paso 2
+    const verifyToken = async () => {
+      try {
+        const url = `http://localhost:3000/api/confirmar/${token}` 
+        const respuesta = await axios.get(url)
+        setMensaje({respuesta:respuesta.data.msg,tipo:false})
+      } catch (error) {
+        setMensaje({respuesta:error.response.data.msg , tipo:true})
+      }
+    }
+    //cuando se carga se hace eso del envio del toke y verificacion 
+    useEffect(()=>{
+        verifyToken()
+    },[])
+    
+    return (
+        
+        <div className="flex flex-col items-center justify-center">
+        {Object.keys(mensaje).length>0 && <Mensaje tipo={mensaje.tipo}>{mensaje.respuesta}</Mensaje> }
+            <img class="object-cover h-80 w-80 rounded-full border-4 border-solid border-slate-600" src={logoDog} alt="image description"/>
+
+            <div className="flex flex-col items-center justify-center">
+                <p className="text-3xl md:text-4xl lg:text-5xl text-gray-800 mt-12">Muchas Gracias</p>
+                <p className="md:text-lg lg:text-xl text-gray-600 mt-8">Ya puedes iniciar sesión</p>
+                <Link to="/login" className="p-3 m-5 w-full text-center bg-gray-600 text-slate-300 border rounded-xl hover:scale-110 duration-300 hover:bg-gray-900 hover:text-white">Login</Link>
+            </div>
+
+        </div>
+    )
+}
